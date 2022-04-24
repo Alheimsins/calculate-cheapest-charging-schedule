efd21a3b06a932ea0912d919d722fc7ba7480ba4
Geir Gåsodden <geir.gasodden@pythonia.no>
2022-04-24 10:24:33 +0200

    Updates README. Sorts schedule

--- README.md
+++ README.md
@@ -2,6 +2,22 @@
 
 Simple module for calculating the cheapest charging schedule for your EV
 
+# Usage
+
+```JavaScript
+
+import calculateCheapestChargingSchedule from '@alheimsins/calculate-cheapest-charging-schedule'
+import priceArray from './data/pricelist.mjs'
+const batteryCapacity = 38
+const batteryCurrentLevel = 0.75
+const chargingPower = 3.5
+
+console.log(calculateCheapestChargingSchedule({ batteryCapacity, batteryCurrentLevel, chargingPower, priceArray }))
+
+// returns ['2022-04-24T04:00:00.000+02:00', '2022-04-24T13:00:00.000+02:00', '2022-04-24T14:00:00.000+02:00']
+
+```
+
 # License
 
 [MIT](LICENSE)
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import sortByPrice from './lib/sort-by-price.mjs'
+import calculateChargingTime from './lib/calculate-charging-time.mjs'
 
 function calculateCheapestChargingSchedule ({ batteryCapacity, batteryCurrentLevel, chargingPower, priceArray }) {
+  const hoursToCharge = calculateChargingTime({ batteryCapacity, batteryCurrentLevel, chargingPower })
   const sortedSchedule = sortByPrice(priceArray)
-  const cheapestChargingTimes = sortedSchedule.map(item => item.startAt)
-  return cheapestChargingTimes
+  const cheapestChargingTimes = sortedSchedule.map(item => item.startsAt)
+  return cheapestChargingTimes.slice(0, hoursToCharge).sort()
 }
 
 export default calculateCheapestChargingSchedule
