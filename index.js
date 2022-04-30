1ac1c7ccd9c1e736faa36af577bacb8f1e949ba9
Geir Gåsodden <geir.gasodden@pythonia.no>
2022-04-30 10:50:43 +0200

    implements repackPriceArray (minor)

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import sortByPrice from './lib/sort-by-price.mjs'
 import calculateChargingTime from './lib/calculate-charging-time.mjs'
+import repackPriceArray from './lib/repack-price-array.mjs'
 
-function calculateCheapestChargingSchedule ({ batteryCapacity, batteryCurrentLevel, chargingPower, priceArray }) {
+function calculateCheapestChargingSchedule ({ batteryCapacity, batteryCurrentLevel, chargingPower, priceArray, propertyMap }) {
   const hoursToCharge = calculateChargingTime({ batteryCapacity, batteryCurrentLevel, chargingPower })
-  const sortedSchedule = sortByPrice(priceArray)
+  const sortedSchedule = sortByPrice(repackPriceArray(priceArray, propertyMap))
   const cheapestChargingTimes = sortedSchedule.map(item => item.startsAt)
   return cheapestChargingTimes.slice(0, hoursToCharge).sort()
 }
